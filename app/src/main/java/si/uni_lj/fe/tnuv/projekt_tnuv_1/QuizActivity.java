438078650270304a3d9b2b07cbbfab5f43845115
simp <80212488+simp16@users.noreply.github.com>
2024-05-09 22:28:41 +0200

    Added 'getValueFromUserData' and 'saveUserData' for saving user data to local storage

--- app/src/main/java/si/uni_lj/fe/tnuv/projekt_tnuv_1/QuizActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/projekt_tnuv_1/QuizActivity.java
@@ -10,6 +10,15 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.content.Intent;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class QuizActivity extends AppCompatActivity {
 
     private ProgressBar quizProgressBar;
@@ -19,8 +28,8 @@ public class QuizActivity extends AppCompatActivity {
     private Button answerButton1;
     private Button answerButton2;
     private Button answerButton3;
-
     private DatePicker datePick;
+    JSONObject user_json = new JSONObject();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,6 +72,9 @@ public class QuizActivity extends AppCompatActivity {
                 // Create an Intent to start PortfolioActivity
 //                Intent intent = new Intent(QuizActivity.this, PortfolioActivity.class);
 //                startActivity(intent);
+                saveUserData(user_json, "birth-year", datePick.getYear());
+                saveUserData(user_json, "birth-day", datePick.getDayOfMonth());
+                saveUserData(user_json, "birth-month", datePick.getMonth() + 1);
 
             }
         });
@@ -72,6 +84,9 @@ public class QuizActivity extends AppCompatActivity {
                 // Handle answer button 1 click
                 // Update progress bar
                 quizProgressBar.setProgress(quizProgressBar.getProgress() + 10);
+
+                answerButton1.setText(getValueFromUserData("birth-year"));
+
                 // Check if progress bar is full
                 if (quizProgressBar.getProgress() >= 100) {
                     // Start PortfolioActivity
@@ -112,4 +127,42 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
     }
+
+    public String getValueFromUserData(String key) {
+        String value = "";
+        try {
+            File file = new File(getFilesDir(), "user_data.json");
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            StringBuilder stringBuilder = new StringBuilder();
+            String line = bufferedReader.readLine();
+            while (line != null) {
+                stringBuilder.append(line).append("\n");
+                line = bufferedReader.readLine();
+            }
+            bufferedReader.close();
+            String jsonString = stringBuilder.toString();
+
+            JSONObject jsonObject = new JSONObject(jsonString);
+            value = jsonObject.getString(key);
+
+            fileReader.close();
+        } catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+        return value;
+    }
+
+    public void saveUserData(JSONObject json, String key, int value) {
+        try {
+            json.put(key, value);
+            String jsonString = json.toString();
+            FileWriter fileWriter = new FileWriter(new File(getFilesDir(), "user_data.json"));
+            fileWriter.write(jsonString);
+            fileWriter.close();
+        } catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
