3f1cd2b461aa15b83efb95bbbc1f3e3a8ec2b5a2
simp <80212488+simp16@users.noreply.github.com>
2024-06-02 20:36:45 +0200

    Fixed intent to main activity

--- app/src/main/java/si/uni_lj/fe/tnuv/projekt_tnuv_1/LoginActivity.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/projekt_tnuv_1/LoginActivity.java
@@ -75,7 +75,7 @@ public class LoginActivity extends AppCompatActivity {
                         //Toast.makeText(LoginActivity.this, "Authentication successful.",
                         //        Toast.LENGTH_SHORT).show();
                         // Navigate to next activity here
-                        Intent intent=new Intent(LoginActivity.this, PortfolioActivity.class);
+                        Intent intent=new Intent(LoginActivity.this, MainActivity.class);
                         startActivity(intent);
                     } else {
                         Exception e = task.getException();
