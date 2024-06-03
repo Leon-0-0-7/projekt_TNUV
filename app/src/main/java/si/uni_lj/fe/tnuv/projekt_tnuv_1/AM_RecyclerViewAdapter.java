6c7f5621e78206dd104607cd90809ada423edfa9
simp <80212488+simp16@users.noreply.github.com>
2024-06-03 18:48:45 +0200

    Added popup for updating current values

--- app/src/main/java/si/uni_lj/fe/tnuv/projekt_tnuv_1/AM_RecyclerViewAdapter.java
+++ app/src/main/java/si/uni_lj/fe/tnuv/projekt_tnuv_1/AM_RecyclerViewAdapter.java
@@ -1,13 +1,17 @@
 package si.uni_lj.fe.tnuv.projekt_tnuv_1;
 
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,6 +20,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -47,7 +53,7 @@ public class AM_RecyclerViewAdapter extends RecyclerView.Adapter<AM_RecyclerView
         holder.isFirstPopulation = true;
         holder.tvAssetName.setText(assetModels.get(position).getAssetName());
         holder.tvTargetValue.setText(String.valueOf(assetModels.get(position).getTargetValue()));
-        holder.etCurrentValue.setText(String.valueOf(assetModels.get(position).getCurrentValue()));
+        holder.assetValueEditText.setText(String.valueOf(assetModels.get(position).getCurrentValue()));
     }
 
     @Override
@@ -58,33 +64,75 @@ public class AM_RecyclerViewAdapter extends RecyclerView.Adapter<AM_RecyclerView
     class ViewHolder extends RecyclerView.ViewHolder {
         boolean isFirstPopulation;
         TextView tvAssetName, tvTargetValue;
-        EditText etCurrentValue, assetValueEditText;
+        TextView assetValueEditText;
 
         ViewHolder(View itemView) {
             super(itemView);
             tvAssetName = itemView.findViewById(R.id.tvAsset);
             tvTargetValue = itemView.findViewById(R.id.tvTargetValue);
-            etCurrentValue = itemView.findViewById(R.id.etCurrentValue);
             assetValueEditText = itemView.findViewById(R.id.etCurrentValue);
-
-            assetValueEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            assetValueEditText.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onFocusChange(View v, boolean hasFocus) {
-                    int position = getAdapterPosition();
-                    AssetModel assetModel = assetModels.get(position);
-                    String assetName = assetModel.getAssetName();
-                    int newValue;
-                    if (assetValueEditText.getText().toString().isEmpty()) {
-                        newValue = 0;
-                    } else {
-                        newValue = Integer.parseInt(assetValueEditText.getText().toString());
-                    }
-                    assetModel.setCurrentValue(newValue);
-                    userPortfolio.put(assetName, newValue);
-                    // Update all the values in the Firestore
-                    updateFirestore(userPortfolio);
+                public void onClick(View v) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    // Inflate the custom layout
+                    LayoutInflater inflater = LayoutInflater.from(context);
+                    View dialogView = inflater.inflate(R.layout.current_value_dialog, null);
+                    // Set the custom layout to the AlertDialog builder
+                    builder.setView(dialogView);
+                    // Get the EditText and ImageViews from the custom layout
+                    EditText input = dialogView.findViewById(R.id.dialog_edit_text);
+                    ImageView crossImage = dialogView.findViewById(R.id.cross_image);
+                    ImageView arrowImage = dialogView.findViewById(R.id.arrow_image);
+
+                    // Create and show the AlertDialog
+                    AlertDialog dialog = builder.create();
+                    Objects.requireNonNull(dialog.getWindow()).setBackgroundDrawableResource(R.drawable.rounded_dialog);
+                    // Set an OnClickListener on the cross ImageView
+                    crossImage.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            // Dismiss the dialog
+                            dialog.dismiss();
+                        }
+                    });
+                    // Set an OnClickListener on the arrow ImageView
+                    arrowImage.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            int position = getBindingAdapterPosition();
+                            AssetModel assetModel = assetModels.get(position);
+                            String assetName = assetModel.getAssetName();
+                            int newValue;
+                            if (input.getText().toString().isEmpty()) {
+                                newValue = 0;
+                            } else {
+                                newValue = Integer.parseInt(input.getText().toString());
+                            }
+                            assetModel.setCurrentValue(newValue);
+                            userPortfolio.put(assetName, newValue);
+                            assetValueEditText.setText(String.valueOf(newValue));
+                            // Update all the values in the Firestore
+                            updateFirestore(userPortfolio);
+                            // Dismiss the dialog
+                            dialog.dismiss();
+                        }
+                    });
+                    input.setText(assetValueEditText.getText());
+                    // Request focus on the EditText and show the keyboard
+                    dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+                    input.requestFocus();
+                    input.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            InputMethodManager inputMethodManager = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+                            inputMethodManager.showSoftInput(input, InputMethodManager.SHOW_IMPLICIT);
+                        }
+                    });
+                    dialog.show();
                 }
             });
+
         }
 
         private void updateFirestore(Map<String, Integer> userPortfolio) {
@@ -97,7 +145,7 @@ public class AM_RecyclerViewAdapter extends RecyclerView.Adapter<AM_RecyclerView
             db.collection("users").document(userId).update(data).addOnCompleteListener(task -> {
                 if (task.isSuccessful()) {
                     if (isFirstPopulation) {
-                        Toast.makeText(context, "Portfolio updated", Toast.LENGTH_SHORT).show();
+//                        Toast.makeText(context, "Portfolio updated", Toast.LENGTH_SHORT).show();
                         isFirstPopulation = false;
                     }
                 } else {
